6e8b250a94edf6bb7ea98f7688c1b2e62ba65c03
zawkindev <zawkindev@gmail.com>
2024-09-26 00:09:21 +0500

    feat: get words from db

--- src/main/java/zawkin/asuna/model/WordModel.java
+++ src/main/java/zawkin/asuna/model/WordModel.java
@@ -1,13 +1,36 @@
 package zawkin.asuna.model;
 
 public class WordModel {
+    private int id;
     private String name;
     private String translation;
-
 
     public WordModel(String name, String translation) {
         this.name = name;
         this.translation = translation;
+    }
+
+    public WordModel(int id, String name, String translation) {
+        this.id = id;
+        this.name = name;
+        this.translation = translation;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
--- src/main/java/zawkin/asuna/repository/WordsRepository.java
+++ src/main/java/zawkin/asuna/repository/WordsRepository.java
@@ -1,8 +1,11 @@
 package zawkin.asuna.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.BeanPropertyBindingResult;
 import zawkin.asuna.container.ComponentContainer;
 import zawkin.asuna.model.WordModel;
 
@@ -10,6 +13,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 @Repository
@@ -17,56 +21,28 @@ public class WordsRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public HashMap<String, String> getWordlist() {
-        String fileData = readFile();
-        HashMap<String, String> dictionary = new HashMap<>();
-        String previousWord = "";
-        String currentWord = "";
-        char c;
-        for (int i = 0; i < fileData.length(); i++) {
-            c = fileData.charAt(i);
-            switch (c) {
-                case ',':
-                    previousWord = currentWord;
-                    currentWord = "";
-                    break;
-                case '\n':
-                    dictionary.put(previousWord, currentWord);
-                    previousWord = "";
-                    currentWord = "";
-                    break;
-                default:
-                    currentWord += Character.toString(c);
-            }
-        }
-        return dictionary;
-    }
-
-    public String readFile() {
-        String data = "";
-        try {
-            Scanner reader = new Scanner(ComponentContainer.csvFile);
-            while (reader.hasNextLine()) {
-                data += reader.nextLine() + "\n";
-            }
-            reader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occured");
-            e.printStackTrace();
-        }
-        return data;
-    }
-
-    public void addWord(WordModel word) {
-        String sql = "insert into dictionary (eng, uzb) values(?, ?)";
-        jdbcTemplate.update(sql, word.getName(), word.getTranslation());
-    }
-
     public void createDictionaryTable() {
         String sql = "create table if not exists dictionary("
                 + "id serial primary key,"
                 + "eng varchar,"
                 + "eng varchar);";
         jdbcTemplate.update(sql);
+    }
+
+    public HashMap<String, String> getWordlist() {
+        HashMap<String, String> dictionary = new HashMap<>();
+        String sql = "select * from dictionary";
+        List<WordModel> words = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(WordModel.class));
+
+        for (WordModel w : words) {
+            dictionary.put(w.getName(), w.getTranslation());
+        }
+
+        return dictionary;
+    }
+
+    public void addWord(WordModel word) {
+        String sql = "insert into dictionary (eng, uzb) values(?, ?)";
+        jdbcTemplate.update(sql, word.getName(), word.getTranslation());
     }
 }
